b48433a2f16808802fa5f5e4e0dd4a577ff88402
Stephen Harwick <sdharwick@gmail.com>
2018-02-28 22:16:00 -0500

    Steps

---
+++ steps/index.js
@@ -0,0 +1,104 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+function steps(n, row = 0, stair = '') {
+	if ( n === row) {
+		return;
+	}
+
+	if (n === stair.length) {
+		console.log(stair);
+		steps(n, row + 1);
+		return;
+	}
+		// or 
+		// return steps(n, row + 1);
+
+	const add = stair.length <= row ? '#' : ' ';
+	steps(n, row, stair + add);
+}
+
+module.exports = steps;
+
+
+
+
+
+// Solution 1
+
+// function steps(n) {
+// 	for (let row = 0; row < n; row++) {
+// 		let stair = '';
+
+// 		for (let column = 0; column < n; column++) {
+// 			if (column <= row) {
+// 				stair += '#';
+// 			} else {
+// 				stair += ' ';
+// 			}
+// 		}
+// 		console.log(stair);
+// 	}
+// }
+
+
+// Solution 2
+
+// function steps(n, row = 0, stair = '') {
+// 	if ( n === row) {
+// 		return;
+// 	}
+
+// 	if (n === stair.length) {
+// 		console.log(stair);
+// 		steps(n, row + 1);
+// 		return;
+// 	}
+// 		// or 
+// 		// return steps(n, row + 1);
+
+// 		if (stair.length <= row) {
+// 			stair += '#';
+// 		} else {
+// 			stair += ' ';
+// 		}
+// 	 steps(n, row, stair);
+// }
+
+
+// Solution 2 Slight Refactor
+
+// function steps(n, row = 0, stair = '') {
+// 	if ( n === row) {
+// 		return;
+// 	}
+
+// 	if (n === stair.length) {
+// 		console.log(stair);
+// 		steps(n, row + 1);
+// 		return;
+// 	}
+// 		// or 
+// 		// return steps(n, row + 1);
+
+// 	const add = stair.length <= row ? '#' : ' ';
+// 	steps(n, row, stair + add);
+// }
+
+
