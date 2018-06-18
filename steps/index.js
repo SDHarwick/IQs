b2dd7387d82227f69f9254ffe76efad81fa51d4b
Stephen Harwick <sdharwick@gmail.com>
2018-06-18 18:00:45 -0400

    Additional Comments

--- steps/index.js
+++ steps/index.js
@@ -58,6 +58,7 @@ module.exports = steps;
 // }
 
 
+
 // Solution 2
 
 // function steps(n, row = 0, stair = '') {
--- vowels/index.js
+++ vowels/index.js
@@ -9,9 +9,28 @@
 
 function vowels(str) {
 
+// g makes you find all instances rather than stopping after the first one
+// i make the regEx case insensitive
 
-
+	const matches = str.match(/[aeiou]/gi);
+	return matches ? matches.length : 0;
 
 }
 
 module.exports = vowels;
+
+// // Solution 1
+
+// function vowels(str) {
+
+// 	let count = 0;
+// 	const vowelCheck = ['a', 'e', 'i', 'o', 'u'];
+// 	for (let char of str.toLowerCase()) {
+// 		if (vowelCheck.includes(char)) {
+// 			count++; 
+// 		}
+// 	}
+
+// 	return count;
+
+// }
